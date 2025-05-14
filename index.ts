c6c90bfa60b00f15b410d266113b0bbe76144eb4
Nata <natastrr@yandex.ru>
2025-05-15 00:16:21 +0400

    init

---
+++ index.ts
@@ -0,0 +1,257 @@
+const translations: Record<LanguageType, LangData> = {
+    en: {
+        name: 'Dato Gogitidze',
+        position: 'Financial Manager',
+        location: 'Georgia, Batumi',
+        education: 'Education',
+        educationDescription: '<span>International Black Sea University</span> – Business Administration, Financial Management',
+        experience: 'Experience',
+        experienceTitle1: 'Self-trader',
+        experienceDescription1: 'Trading on the forex market with currency and commodity pairs;<br>Trading cryptocurrency (CEX/DEX) under NDA agreement',
+        experienceTitle2: '<span>LLC Consulting Group</span> – Financial Manager',
+        experienceDescription2: `
+            <li>Monitoring of client trading</li>
+            <li>Writing daily/weekly report</li>
+            <li>Opening trading accounts</li>
+            <li>Trading on financial markets with various instruments</li>
+            <li>Trading with bots using various strategies</li>
+            <li>Operations in cryptocurrency, P2P transfers, staking</li>
+            <li>Customer communication support</li>
+            <li>Client consulting / Relationship management</li>
+            <li>Analysis/forecasting of international financial markets</li>
+        `,
+        skills: 'Skills',
+        skillsTitle1: '<span>Professional skills</span>',
+        skillsDescription1: `
+            <li>Spot trading</li>
+            <li>Futures trading</li>
+            <li>Portfolio investings</li>
+            <li>Investing ICO / IDO / IEO / Pools</li>
+            <li>NFT / Airdrop / Retrodrop</li>
+            <li>Multi-accounting / Accounts for sale (OTC chats)</li>
+            <li>Staking / Farming / Play-to-earn / Move-to-earn</li>
+            <li>Trading bot (Veles)</li>
+        `,
+        skillsTitle2: '<span>Languages</span>',
+        skillsDescription2: `
+            <li><span class="skill">English</span> – Upper-Intermediate</li>
+            <li><span class="skill">Georgian</span> – Full Proficiency</li>
+            <li><span class="skill">Russian</span> – Full Proficiency</li>
+            <li><span class="skill">Turkish</span> – Beginner</li>
+        `,
+    },
+    ru: {
+        name: 'Дато Гогитизде',
+        position: 'Финансовый менеджер',
+        location: 'Грузия, Бутуми',
+        education: 'Образование',
+        educationDescription: '<span>Международный Черноморский университет</span> – Бизнес-администрирование, финансовый менеджмент',
+        experience: 'Опыт',
+        experienceTitle1: 'Самостоятельная торговля',
+        experienceDescription1: 'Торговля на рынке forex валютными и товарными парами;<br>Торговля криптовалютой (CEX/DEX) в рамках соглашения о неразглашении информации',
+        experienceTitle2: '<span>ООО Консалтинговая Группа</span> – Финансовый менеджер',
+        experienceDescription2: `
+            <li>Мониторинг клиентской торговли</li>
+            <li>Написание ежедневного/еженедельного отчета</li>
+            <li>Открытие торговых счетов</li>
+            <li>Торговля на финансовых рынках с использованием различных инструментов</li>
+            <li>Торговля с ботами с использованием различных стратегий</li>
+            <li>Операции с криптовалютой, P2P-переводы, стейкинг</li>
+            <li>Поддержка общения с клиентами</li>
+            <li>Консультирование клиентов / Управление взаимоотношениями</li>
+            <li>Анализ/прогнозирование международных финансовых рынков</li>
+        `,
+        skills: 'Навыки',
+        skillsTitle1: '<span>Профессиональные навыки</span>',
+        skillsDescription1: `
+            <li>Спотовая торговля</li>
+            <li>Торговля фьючерсами</li>
+            <li>Портфельные инвестиции</li>
+            <li>Инвестирование ICO / IDO / IEO / Пулы</li>
+            <li>NFT / Airdrop / Retrodrop</li>
+            <li>Мультиаккаунтинг / Аккаунты на продажу (OTC чаты)</li>
+            <li>Стэйкинг / Фарминг / Play-to-earn / Move-to-earn</li>
+            <li>Торговый бот (Veles)</li>
+        `,
+        skillsTitle2: '<span>Языки</span>',
+        skillsDescription2: `
+            <li><span class="skill">Английский</span> – Выше среднего</li>
+            <li><span class="skill">Грузинский</span> – Полное владение</li>
+            <li><span class="skill">Русский</span> – Полное владение</li>
+            <li><span class="skill">Турецкий</span> – Начинающий</li>
+        `,
+    },
+    ge: {
+        name: 'დათო გოგიტიძე',
+        position: 'ფინანსური მენეჯერი',
+        location: 'საქართველო, ბათუმი',
+        education: 'განათლება',
+        educationDescription: '<span>შავი ზღვის საერთაშორისო უნივერსიტეტი</span> – ბიზნესის ადმინისტრირება, ფინანსური მენეჯმენტი',
+        experience: 'გამოცდილება',
+        experienceTitle1: 'თვით მოვაჭრე',
+        experienceDescription1: 'ვაჭრობა ფორექსის ბაზარზე ვალუტისა და სასაქონლო წყვილებით;<br>ვაჭრობა კრიპტოვალუტით (CEX/DEX) NDA ხელშეკრულებით',
+        experienceTitle2: '<span>შპს საკონსულტაციო ჯგუფი</span> - ფინანსური მენეჯერი',
+        experienceDescription2: `
+            <li>კლიენტებთან ვაჭრობის მონიტორინგი</li>
+            <li>ყოველდღიური/კვირეული ანგარიშის წერა</li>
+            <li>სავაჭრო ანგარიშების გახსნა</li>
+            <li>ვაჭრობა ფინანსურ ბაზრებზე სხვადასხვა ინსტრუმენტებით</li>
+            <li>ვაჭრობა ბოტებთან სხვადასხვა სტრატეგიის გამოყენებით</li>
+            <li>ოპერაციები კრიპტოვალუტაში, P2P გადარიცხვები, ფსონინგი</li>
+            <li>მომხმარებელთა კომუნიკაციის მხარდაჭერა</li>
+            <li>კლიენტთა კონსულტაცია / ურთიერთობის მენეჯმენტი</li>
+            <li>საერთაშორისო ფინანსური ბაზრების ანალიზი/პროგნოზირება</li>
+        `,
+        skills: 'უნარები',
+        skillsTitle1: '<span>პროფესიული უნარები</span>',
+        skillsDescription1: `
+            <li>ადგილზე ვაჭრობა</li>
+            <li>ფიუჩერსებით ვაჭრობა</li>
+            <li>პორტფელის ინვესტიციები</li>
+            <li>ინვესტიცია ICO / IDO / IEO / Pools</li>
+            <li>NFT / Airdrop / Retrodrop</li>
+            <li>მრავალსაანგარიშო / ანგარიშები იყიდება (OTC ჩეთები)</li>
+            <li>დადგომა / ფერმერობა / თამაში-მოპოვება / გადაადგილება-მოპოვება</li>
+            <li>სავაჭრო ბოტი (Veles)</li>
+        `,
+        skillsTitle2: '<span>ენები</span>',
+        skillsDescription2: `
+            <li><span class="skill">ინგლისური</span> – ზედა-შუალედური</li>
+            <li><span class="skill">ქართული</span> – სრული ცოდნა</li>
+            <li><span class="skill">რუსული</span> – სრული ცოდნა</li>
+            <li><span class="skill">თურქული</span> – დამწყები</li>
+        `,
+    }
+};
+
+type LanguageType = 'en' | 'ru' | 'ge';
+interface LangData {
+    name: string,
+    position: string,
+    location: string,
+    education: string,
+    educationDescription: string,
+    experience: string,
+    experienceTitle1: string,
+    experienceDescription1: string,
+    experienceTitle2: string,
+    experienceDescription2: string,
+    skills: string,
+    skillsTitle1: string,
+    skillsDescription1: string,
+    skillsTitle2: string,
+    skillsDescription2: string,
+}
+
+class Data {
+    private pageTitle: HTMLTitleElement | null = null;
+    private nameElement: HTMLElement | null = null;
+    private positionElement: HTMLElement | null = null;
+    private locationElement: HTMLElement | null = null;
+    private educationElement: HTMLElement | null = null;
+    private educationDescriptionElement: HTMLElement | null = null;
+    private experienceElement: HTMLElement | null = null;
+    private experienceTitleElement1: HTMLElement | null = null;
+    private experienceDescriptionElement1: HTMLElement | null = null;
+    private experienceTitleElement2: HTMLElement | null = null;
+    private experienceDescriptionElement2: HTMLElement | null = null;
+    private skillsElement: HTMLElement | null = null;
+    private skillsTitleElement1: HTMLElement | null = null;
+    private skillsDescriptionElement1: HTMLElement | null = null;
+    private skillsTitleElement2: HTMLElement | null = null;
+    private skillsDescriptionElement2: HTMLElement | null = null;
+    private changeLanguageButton: HTMLButtonElement | null = null;
+    private language: LanguageType = 'en';
+
+    constructor() {
+        this.getElements();
+        this.setLanguage();
+        this.fillData();
+        this.changeLanguageButtonClick();
+    }
+
+    private setLanguage(): void {
+        const language: string | null = localStorage.getItem('language');
+        if (language !== null && (language === 'en' || language === 'ru' || language === 'ge')) {
+            this.language = language;
+            if (this.changeLanguageButton) this.changeLanguageButton.innerText = this.language;
+        }
+    }
+
+    private getElements(): void {
+        this.pageTitle = document.getElementById('page-title') as HTMLTitleElement;
+        this.nameElement = document.getElementById('name');
+        this.positionElement = document.getElementById('position');
+        this.locationElement = document.getElementById('location');
+        this.educationElement = document.getElementById('education');
+        this.educationDescriptionElement = document.getElementById('education-description');
+        this.experienceElement = document.getElementById('experience');
+        this.experienceTitleElement1 = document.getElementById('experience-title-1');
+        this.experienceDescriptionElement1 = document.getElementById('experience-description-1');
+        this.experienceTitleElement2 = document.getElementById('experience-title-2');
+        this.experienceDescriptionElement2 = document.getElementById('experience-description-2');
+        this.skillsElement = document.getElementById('skills');
+        this.skillsTitleElement1 = document.getElementById('skills-title-1');
+        this.skillsDescriptionElement1 = document.getElementById('skills-description-1');
+        this.skillsTitleElement2 = document.getElementById('skills-title-2');
+        this.skillsDescriptionElement2 = document.getElementById('skills-description-2');
+        this.changeLanguageButton = document.getElementById('change-language-button') as HTMLButtonElement;
+    }
+
+    private changeLanguageButtonClick(): void {
+        if (!this.changeLanguageButton) return;
+        this.changeLanguageButton.onclick = () => {
+            if (!this.changeLanguageButton) return;
+
+            switch (this.language) {
+                case 'en':
+                    this.language = 'ru';
+                    break;
+                case 'ru':
+                    this.language = 'ge';
+                    break;
+                case 'ge':
+                    this.language = 'en';
+                    break;
+            }
+            localStorage.setItem('language', this.language);
+            this.changeLanguageButton.innerText = this.language;
+            this.fillData();
+        }
+    }
+
+    private fillData(): void {
+        this.fillText(this.pageTitle, 'name');
+        this.fillText(this.nameElement, 'name');
+        this.fillText(this.positionElement, 'position');
+        this.fillText(this.locationElement, 'location');
+        this.fillText(this.educationElement, 'education');
+        this.fillHTML(this.educationDescriptionElement, 'educationDescription');
+        this.fillText(this.experienceElement, 'experience');
+        this.fillText(this.experienceTitleElement1, 'experienceTitle1');
+        this.fillHTML(this.experienceDescriptionElement1, 'experienceDescription1');
+        this.fillHTML(this.experienceTitleElement2, 'experienceTitle2');
+        this.fillHTML(this.experienceDescriptionElement2, 'experienceDescription2');
+        this.fillText(this.skillsElement, 'skills');
+        this.fillHTML(this.skillsTitleElement1, 'skillsTitle1');
+        this.fillHTML(this.skillsDescriptionElement1, 'skillsDescription1');
+        this.fillHTML(this.skillsTitleElement2, 'skillsTitle2');
+        this.fillHTML(this.skillsDescriptionElement2, 'skillsDescription2');
+    }
+
+    private fillText(element: HTMLElement | null, key: keyof LangData): void {
+        if (element) {
+            element.innerText = translations[this.language][key];
+        }
+    }
+
+    private fillHTML(element: HTMLElement | null, key: keyof LangData): void {
+        if (element) {
+            element.innerHTML = translations[this.language][key];
+        }
+    }
+}
+
+window.onload = () => {
+    new Data();
+};
---
+++ js/index.js
@@ -0,0 +1,232 @@
+"use strict";
+const translations = {
+    en: {
+        name: 'Dato Gogitidze',
+        position: 'Financial Manager',
+        location: 'Georgia, Batumi',
+        education: 'Education',
+        educationDescription: '<span>International Black Sea University</span> – Business Administration, Financial Management',
+        experience: 'Experience',
+        experienceTitle1: 'Self-trader',
+        experienceDescription1: 'Trading on the forex market with currency and commodity pairs,<br>Trading cryptocurrency (CEX/DEX) under NDA agreement',
+        experienceTitle2: '<span>LLC Consulting Group</span> – Financial Manager',
+        experienceDescription2: `
+            <li>Monitoring of client trading</li>
+            <li>Writing daily/weekly report</li>
+            <li>Opening trading accounts</li>
+            <li>Trading on financial markets with various instruments</li>
+            <li>Trading with bots using various strategies</li>
+            <li>Operations in cryptocurrency, P2P transfers, staking</li>
+            <li>Customer communication support</li>
+            <li>Client consulting / Relationship management</li>
+            <li>Analysis/forecasting of international financial markets</li>
+        `,
+        skills: 'Skills',
+        skillsTitle1: '<span>Professional skills</span>',
+        skillsDescription1: `
+            <li>Spot trading</li>
+            <li>Futures trading</li>
+            <li>Portfolio investings</li>
+            <li>Investing ICO / IDO / IEO / Pools</li>
+            <li>NFT / Airdrop / Retrodrop</li>
+            <li>Multi-accounting / Accounts for sale (OTC chats)</li>
+            <li>Staking / Farming / Play-to-earn / Move-to-earn</li>
+            <li>Trading bot (Veles)</li>
+        `,
+        skillsTitle2: '<span>Languages</span>',
+        skillsDescription2: `
+            <li><span class="skill">English</span> – Upper-Intermediate</li>
+            <li><span class="skill">Georgian</span> – Full Proficiency</li>
+            <li><span class="skill">Russian</span> – Full Proficiency</li>
+            <li><span class="skill">Turkish</span> – Beginner</li>
+        `,
+    },
+    ru: {
+        name: 'Дато Гогитизде',
+        position: 'Финансовый менеджер',
+        location: 'Грузия, Бутуми',
+        education: 'Образование',
+        educationDescription: '<span>Международный Черноморский университет</span> – Бизнес-администрирование, финансовый менеджмент',
+        experience: 'Опыт',
+        experienceTitle1: 'Самостоятельная торговля',
+        experienceDescription1: 'Торговля на рынке forex валютными и товарными парами,<br>Торговля криптовалютой (CEX/DEX) в рамках соглашения о неразглашении информации',
+        experienceTitle2: '<span>ООО Консалтинговая Группа</span> – Финансовый менеджер',
+        experienceDescription2: `
+            <li>Мониторинг клиентской торговли</li>
+            <li>Написание ежедневного/еженедельного отчета</li>
+            <li>Открытие торговых счетов</li>
+            <li>Торговля на финансовых рынках с использованием различных инструментов</li>
+            <li>Торговля с ботами с использованием различных стратегий</li>
+            <li>Операции с криптовалютой, P2P-переводы, стейкинг</li>
+            <li>Поддержка общения с клиентами</li>
+            <li>Консультирование клиентов / Управление взаимоотношениями</li>
+            <li>Анализ/прогнозирование международных финансовых рынков</li>
+        `,
+        skills: 'Навыки',
+        skillsTitle1: '<span>Профессиональные навыки</span>',
+        skillsDescription1: `
+            <li>Спотовая торговля</li>
+            <li>Торговля фьючерсами</li>
+            <li>Портфельные инвестиции</li>
+            <li>Инвестирование ICO / IDO / IEO / Пулы</li>
+            <li>NFT / Airdrop / Retrodrop</li>
+            <li>Мультиаккаунтинг / Аккаунты на продажу (OTC чаты)</li>
+            <li>Стэйкинг / Фарминг / Play-to-earn / Move-to-earn</li>
+            <li>Торговый бот (Veles)</li>
+        `,
+        skillsTitle2: '<span>Языки</span>',
+        skillsDescription2: `
+            <li><span class="skill">Английский</span> – Выше среднего</li>
+            <li><span class="skill">Грузинский</span> – Полное владение</li>
+            <li><span class="skill">Русский</span> – Полное владение</li>
+            <li><span class="skill">Турецкий</span> – Начинающий</li>
+        `,
+    },
+    ge: {
+        name: 'დათო გოგიტიძე',
+        position: 'ფინანსური მენეჯერი',
+        location: 'საქართველო, ბათუმი',
+        education: 'განათლება',
+        educationDescription: '<span>შავი ზღვის საერთაშორისო უნივერსიტეტი</span> – ბიზნესის ადმინისტრირება, ფინანსური მენეჯმენტი',
+        experience: 'გამოცდილება',
+        experienceTitle1: 'თვით მოვაჭრე',
+        experienceDescription1: 'ვაჭრობა ფორექსის ბაზარზე ვალუტისა და სასაქონლო წყვილებით,<br>ვაჭრობა კრიპტოვალუტით (CEX/DEX) NDA ხელშეკრულებით',
+        experienceTitle2: '<span>შპს საკონსულტაციო ჯგუფი</span> - ფინანსური მენეჯერი',
+        experienceDescription2: `
+            <li>კლიენტებთან ვაჭრობის მონიტორინგი</li>
+            <li>ყოველდღიური/კვირეული ანგარიშის წერა</li>
+            <li>სავაჭრო ანგარიშების გახსნა</li>
+            <li>ვაჭრობა ფინანსურ ბაზრებზე სხვადასხვა ინსტრუმენტებით</li>
+            <li>ვაჭრობა ბოტებთან სხვადასხვა სტრატეგიის გამოყენებით</li>
+            <li>ოპერაციები კრიპტოვალუტაში, P2P გადარიცხვები, ფსონინგი</li>
+            <li>მომხმარებელთა კომუნიკაციის მხარდაჭერა</li>
+            <li>კლიენტთა კონსულტაცია / ურთიერთობის მენეჯმენტი</li>
+            <li>საერთაშორისო ფინანსური ბაზრების ანალიზი/პროგნოზირება</li>
+        `,
+        skills: 'უნარები',
+        skillsTitle1: '<span>პროფესიული უნარები</span>',
+        skillsDescription1: `
+            <li>ადგილზე ვაჭრობა</li>
+            <li>ფიუჩერსებით ვაჭრობა</li>
+            <li>პორტფელის ინვესტიციები</li>
+            <li>ინვესტიცია ICO / IDO / IEO / Pools</li>
+            <li>NFT / Airdrop / Retrodrop</li>
+            <li>მრავალსაანგარიშო / ანგარიშები იყიდება (OTC ჩეთები)</li>
+            <li>დადგომა / ფერმერობა / თამაში-მოპოვება / გადაადგილება-მოპოვება</li>
+            <li>სავაჭრო ბოტი (Veles)</li>
+        `,
+        skillsTitle2: '<span>ენები</span>',
+        skillsDescription2: `
+            <li><span class="skill">ინგლისური</span> – ზედა-შუალედური</li>
+            <li><span class="skill">ქართული</span> – სრული ცოდნა</li>
+            <li><span class="skill">რუსული</span> – სრული ცოდნა</li>
+            <li><span class="skill">თურქული</span> – დამწყები</li>
+        `,
+    }
+};
+class Data {
+    constructor() {
+        this.pageTitle = null;
+        this.nameElement = null;
+        this.positionElement = null;
+        this.locationElement = null;
+        this.educationElement = null;
+        this.educationDescriptionElement = null;
+        this.experienceElement = null;
+        this.experienceTitleElement1 = null;
+        this.experienceDescriptionElement1 = null;
+        this.experienceTitleElement2 = null;
+        this.experienceDescriptionElement2 = null;
+        this.skillsElement = null;
+        this.skillsTitleElement1 = null;
+        this.skillsDescriptionElement1 = null;
+        this.skillsTitleElement2 = null;
+        this.skillsDescriptionElement2 = null;
+        this.changeLanguageButton = null;
+        this.language = 'en';
+        this.getElements();
+        this.setLanguage();
+        this.fillData();
+        this.changeLanguageButtonClick();
+    }
+    setLanguage() {
+        const language = localStorage.getItem('language');
+        if (language !== null && (language === 'en' || language === 'ru' || language === 'ge')) {
+            this.language = language;
+            if (this.changeLanguageButton)
+                this.changeLanguageButton.innerText = this.language;
+        }
+    }
+    getElements() {
+        this.pageTitle = document.getElementById('page-title');
+        this.nameElement = document.getElementById('name');
+        this.positionElement = document.getElementById('position');
+        this.locationElement = document.getElementById('location');
+        this.educationElement = document.getElementById('education');
+        this.educationDescriptionElement = document.getElementById('education-description');
+        this.experienceElement = document.getElementById('experience');
+        this.experienceTitleElement1 = document.getElementById('experience-title-1');
+        this.experienceDescriptionElement1 = document.getElementById('experience-description-1');
+        this.experienceTitleElement2 = document.getElementById('experience-title-2');
+        this.experienceDescriptionElement2 = document.getElementById('experience-description-2');
+        this.skillsElement = document.getElementById('skills');
+        this.skillsTitleElement1 = document.getElementById('skills-title-1');
+        this.skillsDescriptionElement1 = document.getElementById('skills-description-1');
+        this.skillsTitleElement2 = document.getElementById('skills-title-2');
+        this.skillsDescriptionElement2 = document.getElementById('skills-description-2');
+        this.changeLanguageButton = document.getElementById('change-language-button');
+    }
+    changeLanguageButtonClick() {
+        if (!this.changeLanguageButton)
+            return;
+        this.changeLanguageButton.onclick = () => {
+            if (!this.changeLanguageButton)
+                return;
+            switch (this.language) {
+                case 'en':
+                    this.language = 'ru';
+                    break;
+                case 'ru':
+                    this.language = 'ge';
+                    break;
+                case 'ge':
+                    this.language = 'en';
+                    break;
+            }
+            localStorage.setItem('language', this.language);
+            this.changeLanguageButton.innerText = this.language;
+            this.fillData();
+        };
+    }
+    fillData() {
+        this.fillText(this.pageTitle, 'name');
+        this.fillText(this.nameElement, 'name');
+        this.fillText(this.positionElement, 'position');
+        this.fillText(this.locationElement, 'location');
+        this.fillText(this.educationElement, 'education');
+        this.fillHTML(this.educationDescriptionElement, 'educationDescription');
+        this.fillText(this.experienceElement, 'experience');
+        this.fillText(this.experienceTitleElement1, 'experienceTitle1');
+        this.fillHTML(this.experienceDescriptionElement1, 'experienceDescription1');
+        this.fillHTML(this.experienceTitleElement2, 'experienceTitle2');
+        this.fillHTML(this.experienceDescriptionElement2, 'experienceDescription2');
+        this.fillText(this.skillsElement, 'skills');
+        this.fillHTML(this.skillsTitleElement1, 'skillsTitle1');
+        this.fillHTML(this.skillsDescriptionElement1, 'skillsDescription1');
+        this.fillHTML(this.skillsTitleElement2, 'skillsTitle2');
+        this.fillHTML(this.skillsDescriptionElement2, 'skillsDescription2');
+    }
+    fillText(element, key) {
+        if (element) {
+            element.innerText = translations[this.language][key];
+        }
+    }
+    fillHTML(element, key) {
+        if (element) {
+            element.innerHTML = translations[this.language][key];
+        }
+    }
+}
+window.onload = () => {
+    new Data();
+};
